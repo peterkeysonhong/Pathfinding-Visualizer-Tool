51bbdfbbb97112b4830b2361020acb0645ac50a0
peterhong440 <peterhong440@gmail.com>
2023-11-06 17:50:40 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
 import React from 'react';
-import PathfindingVisualizer from '/Users/peterhong/Desktop/pathfinding-visualizer-tool/src'; 
+import './App.css';
+import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
 /* PathfindingVisualizer is the name of the variable that will be used to reference the default export from the module
-being imported. The second part of the import statement above is the path to the module being imported */
-
+being imported. The second part of the import statement above is the path to the module being imported. This import
+statement above is necessary for me to create a PathfindingVisualizer component for this React program. */
 
 function App() {
   return (
